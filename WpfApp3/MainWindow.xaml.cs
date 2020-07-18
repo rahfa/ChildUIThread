df8e3960bb60d403a4076c5b33690ef70acfae89
Fahimeh Rahmatypoor <rahfa2001@yahoo.com>
2020-07-18 17:55:29 +0430

    intitial commit

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var bw = new BackgroundWorker();
+            bw.DoWork += Bw_DoWork;
+
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+
+            bw.RunWorkerAsync();
+
+        }
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Done!");
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(2000);
+            MessageBox.Show("Continue?","",MessageBoxButton.OKCancel);
+            Thread.Sleep(5000);
+
+        }
+    }
+}
